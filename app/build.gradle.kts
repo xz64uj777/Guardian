7d0baa9d7398d397dc68f4cfea8a80ee0d23de0e
xz64uj777 <xz64uj777@gmail.com>
2026-09-06 08:58:01 -0400

    Bump Guardian to 0.1.2 alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.guardianlayer.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.1-alpha"
+        versionCode = 3
+        versionName = "0.1.2-alpha"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
