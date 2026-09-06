c6f2010d4a128dbf0699530394a571c9624ff114
xz64uj777 <xz64uj777@gmail.com>
2026-09-06 08:50:16 -0400

    Bump Guardian alpha after VPN shutdown fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.guardianlayer.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0-alpha"
+        versionCode = 2
+        versionName = "0.1.1-alpha"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
